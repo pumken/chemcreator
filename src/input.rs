51cb72c3beb733f52aab0782f08e7496f5485112
Gavin Tran <gavinpha@outlook.com>
2023-03-14 01:34:26 -0400

    Extracted input to separate functions.

--- src/input.rs
+++ src/input.rs
@@ -1,3 +1,91 @@
 //! # Input
 //!
 //! The `input` module contains functions that interpret user input.
+
+use ruscii::app::State;
+use ruscii::keyboard::{Key, KeyEvent};
+use ruscii::spatial::Direction;
+use crate::algorithm::debug_chain;
+use crate::{AppState, Mode};
+use crate::molecule::BondOrder::{Double, Single, Triple};
+use crate::molecule::Element::{C, H, O};
+use crate::spatial::GridState;
+
+pub(crate) fn input_insert_mode(app_state: &State, state: &mut AppState, graph: &mut GridState) {
+    for key_event in app_state.keyboard().last_key_events() {
+        match key_event {
+            KeyEvent::Pressed(Key::C) => {
+                graph.put_atom(C);
+                state.menu_key = "C";
+            }
+            KeyEvent::Pressed(Key::H) => {
+                graph.put_atom(H);
+                state.menu_key = "H";
+            }
+            KeyEvent::Pressed(Key::O) => {
+                graph.put_atom(O);
+                state.menu_key = "O";
+            }
+            KeyEvent::Pressed(Key::F5) => {
+                state.menu_debug = match debug_chain(&graph) {
+                    Ok(it) => {
+                        it.iter()
+                            .fold("".to_string(), |a, b| {
+                                format!("{a} ({}, {}),", b.pos.x, b.pos.y)
+                            })
+                    }
+                    Err(it) => it.to_string()
+                };
+            }
+            KeyEvent::Pressed(Key::Num1) => {
+                graph.put_bond(Single);
+                state.menu_key = "1";
+            }
+            KeyEvent::Pressed(Key::Num2) => {
+                graph.put_bond(Double);
+                state.menu_key = "2";
+            }
+            KeyEvent::Pressed(Key::Num3) => {
+                graph.put_bond(Triple);
+                state.menu_key = "3";
+            }
+            KeyEvent::Pressed(Key::Backspace) => {
+                graph.clear();
+                state.menu_key = "Backspace";
+            }
+            KeyEvent::Pressed(Key::Right) => {
+                graph.move_cursor(Direction::Right);
+                state.menu_key = "→";
+            }
+            KeyEvent::Pressed(Key::Left) => {
+                graph.move_cursor(Direction::Left);
+                state.menu_key = "←";
+            }
+            KeyEvent::Pressed(Key::Up) => {
+                graph.move_cursor(Direction::Up);
+                state.menu_key = "↑";
+            }
+            KeyEvent::Pressed(Key::Down) => {
+                graph.move_cursor(Direction::Down);
+                state.menu_key = "↓";
+            }
+            KeyEvent::Pressed(Key::Esc) => state.mode = Mode::Normal,
+            _ => {
+                state.menu_key = match key_event {
+                    KeyEvent::Pressed(_) => { "Pressed" }
+                    KeyEvent::Released(_) => { "" }
+                }
+            }
+        }
+    }
+}
+
+pub(crate) fn input_view_mode(app_state: &State, state: &mut AppState) {
+    for key_event in app_state.keyboard().last_key_events() {
+        match key_event {
+            KeyEvent::Pressed(Key::Esc) => app_state.stop(),
+            KeyEvent::Pressed(Key::F8) => state.mode = Mode::Insert,
+            _ => ()
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use ruscii::spatial::{Direction, Vec2};
 use ruscii::terminal::Color::{Cyan, Red, White};
 use ruscii::terminal::Window;
 use crate::algorithm::{debug_chain, name_molecule};
+use crate::input::{input_insert_mode, input_view_mode};
 
 use crate::spatial::{GridState, Invert};
 use crate::molecule::BondOrder::{Double, Single, Triple};
@@ -29,90 +30,19 @@ fn main() {
     let mut app = App::new();
     let version = env!("CARGO_PKG_VERSION");
     let mut graph = GridState::new(20, 10);
-    let mut mode = Mode::Start;
-    let mut menu_name = "                ChemCreator                ".to_string();
-    let mut menu_pos = "      Written in Rust by Gavin Tran.       ".to_string();
-    let mut menu_sym = "To start, enter insert mode by pressing F8.".to_string();
-    let mut menu_key = "";  // Overridden in Start mode to retain &str type
-    let mut menu_err = "".to_string();
-    let mut menu_debug = "".to_string();
+    let mut state = AppState::default();
 
     app.run(|app_state: &mut State, window: &mut Window| {
-        match mode {
+        match state.mode {
             Mode::Insert => {
-                for key_event in app_state.keyboard().last_key_events() {
-                    match key_event {
-                        KeyEvent::Pressed(Key::C) => {
-                            graph.put_atom(C);
-                            menu_key = "C";
-                        }
-                        KeyEvent::Pressed(Key::H) => {
-                            graph.put_atom(H);
-                            menu_key = "H";
-                        }
-                        KeyEvent::Pressed(Key::O) => {
-                            graph.put_atom(O);
-                            menu_key = "O";
-                        }
-                        KeyEvent::Pressed(Key::F5) => {
-                            menu_debug = match debug_chain(&graph) {
-                                Ok(it) => {
-                                    it.iter()
-                                        .fold("".to_string(), |a, b| {
-                                            format!("{a} ({}, {}),", b.pos.x, b.pos.y)
-                                        })
-                                }
-                                Err(it) => it.to_string()
-                            };
-                        }
-                        KeyEvent::Pressed(Key::Num1) => {
-                            graph.put_bond(Single);
-                            menu_key = "1";
-                        }
-                        KeyEvent::Pressed(Key::Num2) => {
-                            graph.put_bond(Double);
-                            menu_key = "2";
-                        }
-                        KeyEvent::Pressed(Key::Num3) => {
-                            graph.put_bond(Triple);
-                            menu_key = "3";
-                        }
-                        KeyEvent::Pressed(Key::Backspace) => {
-                            graph.clear();
-                            menu_key = "Backspace";
-                        }
-                        KeyEvent::Pressed(Key::Right) => {
-                            graph.move_cursor(Direction::Right);
-                            menu_key = "→";
-                        }
-                        KeyEvent::Pressed(Key::Left) => {
-                            graph.move_cursor(Direction::Left);
-                            menu_key = "←";
-                        }
-                        KeyEvent::Pressed(Key::Up) => {
-                            graph.move_cursor(Direction::Up);
-                            menu_key = "↑";
-                        }
-                        KeyEvent::Pressed(Key::Down) => {
-                            graph.move_cursor(Direction::Down);
-                            menu_key = "↓";
-                        }
-                        KeyEvent::Pressed(Key::Esc) => mode = Mode::Normal,
-                        _ => {
-                            menu_key = match key_event {
-                                KeyEvent::Pressed(_) => { "Pressed" }
-                                KeyEvent::Released(_) => { "" }
-                            }
-                        }
-                    }
-                }
+                input_insert_mode(app_state, &mut state, &mut graph);
 
-                (menu_name, menu_err) = match name_molecule(&graph) {
+                (state.menu_name, state.menu_err) = match name_molecule(&graph) {
                     Ok(it) => (it, "".to_string()),
                     Err(it) => ("unidentified".to_string(), it.to_string()),
                 };
-                menu_pos = format!("({}, {})", graph.cursor.x, graph.cursor.y);
-                menu_sym = match graph.current_cell() {
+                state.menu_pos = format!("({}, {})", graph.cursor.x, graph.cursor.y);
+                state.menu_sym = match graph.current_cell() {
                     Cell::Atom(it) => { format!("Atom {}", it.symbol()) }
                     Cell::Bond(it) => {
                         match it.order {
@@ -125,33 +55,19 @@ fn main() {
                 };
             }
             Mode::Normal => {
-                menu_pos = "".to_string();
-                menu_sym = "".to_string();
-                menu_key = "";
-                menu_err = "".to_string();
-                for key_event in app_state.keyboard().last_key_events() {
-                    match key_event {
-                        KeyEvent::Pressed(Key::Esc) => app_state.stop(),
-                        KeyEvent::Pressed(Key::F8) => mode = Mode::Insert,
-                        _ => ()
-                    }
-                }
+                state.menu_pos = "".to_string();
+                state.menu_sym = "".to_string();
+                state.menu_key = "";
+                state.menu_err = "".to_string();
+                input_view_mode(app_state, &mut state)
             }
-            Mode::Start => {
-                for key_event in app_state.keyboard().last_key_events() {
-                    match key_event {
-                        KeyEvent::Pressed(Key::Esc) => app_state.stop(),
-                        KeyEvent::Pressed(Key::F8) => mode = Mode::Insert,
-                        _ => ()
-                    }
-                }
-            }
+            Mode::Start => input_view_mode(app_state, &mut state),
         }
 
         let mut pencil = Pencil::new(window.canvas_mut());
 
         // Grid and startup screen
-        match mode {
+        match state.mode {
             Mode::Start => {
                 pencil.draw_text("┌────┐          ", Vec2::xy(graph.size.x + 2, graph.size.y / 2 + 2).inv(&graph));
                 pencil.draw_text("│  C │hem       ", Vec2::xy(graph.size.x + 2, graph.size.y / 2 + 1).inv(&graph));
@@ -166,7 +82,7 @@ fn main() {
                     pencil.draw_text(match &cell {
                         Cell::Atom(it) => { it.symbol() }
                         Cell::Bond(it) => { it.symbol() }
-                        Cell::None(_) => match mode {
+                        Cell::None(_) => match state.mode {
                             Mode::Insert => " • ",
                             Mode::Normal => "   ",
                             _ => "   "
@@ -177,7 +93,7 @@ fn main() {
         }
 
         // Insert mode and cursor
-        if let Mode::Insert = mode {
+        if let Mode::Insert = state.mode {
             pencil.draw_text("-- INSERT MODE --", Vec2::xy(graph.size.x * 3 + 3, 1));
             pencil.set_foreground(Cyan);
             pencil.draw_text("<", Vec2::xy(graph.cursor.x * 3 - 1, graph.cursor.y).inv(&graph));
@@ -186,20 +102,20 @@ fn main() {
         }
 
         // Menu
-        pencil.draw_text(match mode {
+        pencil.draw_text(match state.mode {
             Mode::Start => "",
             _ => "ChemCreator"
         }, Vec2::xy(graph.size.x * 3 + 3, 0));
-        pencil.draw_text(&format!("name | {}", menu_name), Vec2::xy(graph.size.x * 3 + 3, 2));
-        pencil.draw_text(&format!(" pos | {}", menu_pos), Vec2::xy(graph.size.x * 3 + 3, 3));
-        pencil.draw_text(&format!(" sym | {}", menu_sym), Vec2::xy(graph.size.x * 3 + 3, 4));
-        pencil.draw_text(&format!(" key | {}", match mode {
+        pencil.draw_text(&format!("name | {}", state.menu_name), Vec2::xy(graph.size.x * 3 + 3, 2));
+        pencil.draw_text(&format!(" pos | {}", state.menu_pos), Vec2::xy(graph.size.x * 3 + 3, 3));
+        pencil.draw_text(&format!(" sym | {}", state.menu_sym), Vec2::xy(graph.size.x * 3 + 3, 4));
+        pencil.draw_text(&format!(" key | {}", match state.mode {
             Mode::Start => format!("        You're using version {version}.          "),
-            _ => menu_key.to_string()
+            _ => state.menu_key.to_string()
         }), Vec2::xy(graph.size.x * 3 + 3, 5));
         pencil.set_foreground(Red);
-        pencil.draw_text(&menu_err.to_string(), Vec2::xy(graph.size.x * 3 + 3, 7));
-        pencil.draw_text(&menu_debug.to_string(), Vec2::xy(graph.size.x * 3 + 3, 8));
+        pencil.draw_text(&state.menu_err.to_string(), Vec2::xy(graph.size.x * 3 + 3, 7));
+        pencil.draw_text(&state.menu_debug.to_string(), Vec2::xy(graph.size.x * 3 + 3, 8));
     });
 }
 
@@ -208,4 +124,28 @@ enum Mode {
     Insert,
     Normal,
     Start,
+}
+
+struct AppState {
+    mode: Mode,
+    menu_name: String,
+    menu_pos: String,
+    menu_sym: String,
+    menu_key: &'static str,
+    menu_err: String,
+    menu_debug: String,
+}
+
+impl Default for AppState {
+    fn default() -> Self {
+        AppState {
+            mode: Mode::Start,
+            menu_name: "                ChemCreator                ".to_string(),
+            menu_pos:  "      Written in Rust by Gavin Tran.       ".to_string(),
+            menu_sym:  "To start, enter insert mode by pressing F8.".to_string(),
+            menu_key: "",  // Overridden in Start mode to retain &str type
+            menu_err: "".to_string(),
+            menu_debug: "".to_string(),
+        }
+    }
 }

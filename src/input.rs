620cc723b8b9ebcd587c82932bcf15bce4c62ed1
Gavin Tran <gavinpha@outlook.com>
2023-03-19 12:54:41 -0700

    Add halogens and nitrogen. (#11)

--- src/input.rs
+++ src/input.rs
@@ -4,7 +4,8 @@
 
 use crate::groups::debug_branches;
 use crate::molecule::BondOrder::{Double, Single, Triple};
-use crate::molecule::Element::{C, H, O};
+use crate::molecule::Element;
+use crate::molecule::Element::{C, H, N, O};
 use crate::spatial::GridState;
 use crate::{AppState, Mode};
 use ruscii::app::State;
@@ -14,13 +15,33 @@ use ruscii::spatial::Direction;
 pub(crate) fn input_insert_mode(app_state: &State, state: &mut AppState, graph: &mut GridState) {
     for key_event in app_state.keyboard().last_key_events() {
         match key_event {
+            KeyEvent::Pressed(Key::B) => {
+                graph.put_atom(Element::Br);
+                state.key = "B";
+            }
             KeyEvent::Pressed(Key::C) => {
                 graph.put_atom(C);
                 state.key = "C";
             }
+            KeyEvent::Pressed(Key::F) => {
+                graph.put_atom(Element::F);
+                state.key = "F";
+            }
             KeyEvent::Pressed(Key::H) => {
                 graph.put_atom(H);
                 state.key = "H";
+            }
+            KeyEvent::Pressed(Key::I) => {
+                graph.put_atom(Element::I);
+                state.key = "I";
+            }
+            KeyEvent::Pressed(Key::L) => {
+                graph.put_atom(Element::Cl);
+                state.key = "L";
+            }
+            KeyEvent::Pressed(Key::N) => {
+                graph.put_atom(N);
+                state.key = "N";
             }
             KeyEvent::Pressed(Key::O) => {
                 graph.put_atom(O);
